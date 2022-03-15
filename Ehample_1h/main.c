b4835b17bea4b90f222487b7719e94a30a4d158c
PranavTyagi0604 <56643003+PranavTyagi0604@users.noreply.github.com>
2022-03-15 15:45:46 +0000

    Update main.c

--- Ehample_1h/main.c
+++ Ehample_1h/main.c
@@ -7,6 +7,16 @@
 #include "hal.h"
 #include "memory_protection.h"
 #include <main.h>
+#include "leds.h"
+#include "spi_comm.h"
+#include "sensors/proximity.h"
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
 
 
 int main(void)
@@ -16,11 +26,37 @@ int main(void)
     chSysInit();
     mpu_init();
 
+    clear_leds();
+    spi_comm_start();
+    set_led(LED1,2);
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    proximity_start();
+    calibrate_ir();
 
+    int amb_light = get_ambient_light(0);
+
+    serial_start();
+
+
+    char str[100];
+    int str_length;
+    int prox;
+    int cal_prox;
+    int x;
+    int y;
     /* Infinite loop. */
     while (1) {
     	//waits 1 second
         chThdSleepMilliseconds(1000);
+
+        prox = get_prox(0);
+        cal_prox = get_calibrated_prox(0);
+        x = sprintf(str, "%d\n", prox);
+        y = sprintf(str, "%d\n", cal_prox);
+
+
+        //str_length = sprintf(str, "Hello World\n");
+        e_send_uart1_char(str, x);
     }
 }
 
